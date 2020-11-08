5d94b723d6afb859afe9d2a0e088c361f9b6cfa9
Elcio Cestari Taira <elciotaira@gmail.com>
2020-11-08 19:46:34 -0400

    Primeiro commit: inicializado implementacao

---
+++ src/main/java/com/elciocestari/gerenciadorbebidas/GerenciadorbebidasApplication.java
@@ -0,0 +1,13 @@
+package com.elciocestari.gerenciadorbebidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorbebidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorbebidasApplication.class, args);
+	}
+
+}
